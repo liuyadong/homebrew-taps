4d425905c05343686fc6dbc911cb6f30e07d86d4
Yadong <liuyadong828@gmail.com>
2022-03-03 14:44:39 +0800

    switch cmake to autotools

--- Formula/hdf4.rb
+++ Formula/hdf4.rb
@@ -4,41 +4,28 @@ class Hdf4 < Formula
   url "https://support.hdfgroup.org/ftp/HDF/releases/HDF4.2.15/src/hdf-4.2.15.tar.gz"
   sha256 "dbeeef525af7c2d01539906c28953f0fdab7dba603d1bc1ec4a5af60d002c459"
 
-  depends_on "cmake" => :build
   depends_on "pkg-config" => :build
+  depends_on "zlib"
   depends_on "jpeg"
   depends_on "szip"
-  depends_on "zlib"
 
   def install
-    ENV.O0 # Per the release notes, -O2 can cause memory corruption
-
-    args = std_cmake_args
-    args.concat [
-      "-DBUILD_SHARED_LIBS=ON",
-      "-DBUILD_TESTING=OFF",
-      "-DHDF4_BUILD_TOOLS=ON",
-      "-DHDF4_BUILD_UTILS=ON",
-      "-DHDF4_BUILD_WITH_INSTALL_NAME=ON",
-      "-DHDF4_ENABLE_JPEG_LIB_SUPPORT=ON",
-      "-DHDF4_ENABLE_NETCDF=OFF", # Conflict. Just install NetCDF for this.
-      "-DHDF4_ENABLE_SZIP_ENCODING=ON",
-      "-DHDF4_ENABLE_SZIP_SUPPORT=ON",
-      "-DHDF4_ENABLE_Z_LIB_SUPPORT=ON",
-      "-DHDF4_BUILD_FORTRAN=OFF",
-      "-DHDF4-BUILD_XDR_LIB=ON"
+    args = [
+      "--prefix=#{prefix}",
+      "--with-zlib=#{Formula["zlib"].prefix}",
+      "--with-jpeg=#{Formula["jpeg"].prefix}",
+      "--with-szip=#{Formula["szip"].prefix}",
+      "--disable-netcdf",
+      "--disable-fortran",
+      "--enable-hdf4-xdr",
     ]
 
-    mkdir "build" do
-      system "cmake", "..", *args
-      system "make", "install"
-      system "make", "test" if build.with? "tests"
+    system "./configure", *args
+    system "make", "install"
 
-      # Remove stray nc* artifacts which conflict with NetCDF.
-      rm (bin+"ncdump")
-      rm (bin+"ncgen")
-      #      rm (include+"netcdf.inc")
-    end
+    # Remove stray nc* artifacts which conflict with NetCDF.
+    rm (bin+"ncdump")
+    rm (bin+"ncgen")
   end
 
   def caveats
